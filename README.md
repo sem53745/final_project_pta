8ed7e0ae7d3ea006fc30e2add61903397b5d6073
sem53745 <s.j.p.bartels@student.rug.nl>
2024-05-25 15:45:44 +0200

    Working on the morphology part of the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # final_project_pta
+
+link presentatie: https://docs.google.com/presentation/d/1kC95nTjriGntkb6pEcW86qXSN1RPnlaJni0SSNvNnRU/edit?usp=sharing
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# Program name: main.py
-# Date: 03/06
-# Contributors: Joris van Bruggen (s5723752), Mervyn Bolhuis (s5119103), Tieme Boerema (s5410762), Jasper Kleine (s5152372), Sem Bartels (s5374588)
-
 import json
 import spacy
+from collections import Counter
 
 nlp = spacy.load("en_core_web_sm")
 
 # sem #
+
 # Read .jsonl files
 def load_jsonl(file_path):
     with open(file_path, 'r') as file:
@@ -18,18 +16,40 @@ human_data = load_jsonl('human.jsonl')
 machine_data = load_jsonl('group1.jsonl')
 
 # Extract text data
-human_texts = [entry['text'] for entry in human_data]
-machine_texts = [entry['text'] for entry in machine_data]
+human_texts = [entry['text'] for entry in human_data if 'text' in entry and entry['text'].strip()]
+machine_texts = [entry['text'] for entry in machine_data if 'text' in entry and entry['text'].strip()]
+
+# Check number of lines with actual text
+print(f'Number of lines with text in human.jsonl: {len(human_texts)}')
+print(f'Number of lines with text in group1.jsonl: {len(machine_texts)}\n')
 
 # Tokenization and Lemmatization
 def tokenize_and_lemmatize(texts):
     tokens = []
     lemmas = []
     for doc in nlp.pipe(texts):
-        tokens.append([token.text for token in doc])
-        lemmas.append([token.lemma_ for token in doc])
+        tokens.extend([token.text for token in doc])
+        lemmas.extend([token.lemma_ for token in doc])
     return tokens, lemmas
 
+# Get tokens and lemmas for human and machine texts
 human_tokens, human_lemmas = tokenize_and_lemmatize(human_texts)
 machine_tokens, machine_lemmas = tokenize_and_lemmatize(machine_texts)
+
+# Average number of tokens and lemmas per line for both the machine and human data
+print(f'Average tokens per line for the human data: {round(len(human_tokens) / len(human_texts), 1)}')
+print(f'Average tokens per line for the machine data: {round(len(machine_tokens) / len(machine_texts), 1)}')
+print(f'Average lemmas per line for the human data: {round(len(human_lemmas) / len(human_texts), 1)}')
+print(f'Average lemmas per line for the machine data: {round(len(machine_lemmas) / len(machine_texts), 1)}\n')
+
+# Looking at the frequency of certain tokens
+human_tokens_count = Counter(human_tokens)
+machine_tokens_count = Counter(machine_tokens)
+print("Human:")
+for item in human_tokens_count.most_common(10):
+    print(item)
+print("\nMachine:")
+for item in machine_tokens_count.most_common(10):
+    print(item)
+
 # sem #
