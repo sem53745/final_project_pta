77c5329d39ad909a612d2151be9d373d107f854a
JasperK04 <j.kleine.4@student.rug.nl>
2024-05-28 23:07:24 +0200

    added a function to predict if a text is AI based on pragmatics

--- pragmatics.py
+++ pragmatics.py
@@ -5,9 +5,76 @@
 
 # Jasper #
 
-from preprocessor import get_and_parse_texts, Path
+from preprocessor import get_and_parse_texts, parse_promt_data, Path
 from spacy.tokens import Doc
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+
+# Jasper #
+DEBUG = False
+
+
+def pragmatic_predictor(text: Doc, comparison: Tuple[float, float, float, float]) -> float:
+    '''
+    Function to predict the pragmatic score of the data
+    param data: Doc, the text to predict the pragmatic score of
+    param comparison: Tuple[float, float, float, float], the comparison values to use
+    '''
+
+    # unpack the comparison values
+    max_sent, min_sent, max_subj, min_subj = comparison
+
+    # get the pragmatic values of the text
+    pragmatic_polarity: float = text._.blob.polarity # type: ignore
+    pragmatic_subjectivity: float = text._.blob.subjectivity # type: ignore
+
+    # check if the pragmatic values are outside the "normal" values with a small margin
+    chance: int = 0
+    if pragmatic_polarity > (max_sent * 1.025) or pragmatic_polarity < (min_sent * 1.025):
+        chance += 1
+    if pragmatic_subjectivity > (max_subj * 1.025) or pragmatic_subjectivity < (min_subj * 1.025):
+        chance += 1
+
+    # return the chance of the text being AI based on the pragmatic values being outside the "normal" values
+    return chance / 2
+
+
+def write_sentiment_results(prompts: List[Dict[str, Doc | str]], comparison_data: Tuple[float, float, float, float]) -> None:
+    '''
+    function to write the sentiment results
+    param prompts: List[Dict[str, Doc | str]], the data to check the sentiment of
+    param comparison_data: Tuple[float, float, float, float], the "norm" values to use
+    '''
+
+    # get the data
+    counter: int = 0
+    AI_texts: int = 0
+    correct_AI: int = 0
+    false_AI: int = 0
+    correct_human: int = 0
+    false_human: int = 0
+    for prompt in prompts:
+        if prompt['by'] == 'AI':
+            AI_texts += 1
+        chance = pragmatic_predictor(prompt['text'], comparison_data) # type: ignore
+        if chance > 0.0:
+            counter += 1
+            if prompt['by'] == 'AI':
+                correct_AI += 1
+            else:
+                false_AI += 1
+        else:
+            if prompt['by'] == 'Human':
+                correct_human += 1
+            else:
+                false_human += 1
+
+    print()
+    print(f'the AI based texts were correctly identified {correct_AI} times out of {AI_texts} \n')
+    print(f'the AI based texts were incorrectly identified as human {false_AI} times out of {AI_texts} \n')
+    print(f'the Human based texts were correctly identified {correct_human} times out of {len(prompts) - AI_texts} \n')
+    print(f'the Human based texts were incorrectly identified as AI {false_human} times out of {len(prompts) - AI_texts} \n')
+    print(f'out of all {len(prompts)} texts {correct_AI + correct_human} were identified correctly \n')
+
 
 def get_sentiment_analysis(data: List[Doc]) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
     '''
@@ -34,14 +101,15 @@ def get_sentiment_analysis(data: List[Doc]) -> Tuple[Tuple[float, float, float],
         if doc._.blob.subjectivity < min_subjectivity:
             min_subjectivity = doc._.blob.subjectivity
         avg_subjectivity += doc._.blob.subjectivity
-  
-    print(f'The maximum positive sentiment is: {max_sentiment:.4f}')
-    print(f'The minimum negative sentiment is: {min_sentiment:.4f}')
-    print(f'The average sentiment is: {avg_sentiment / len(data):.4f}\n')
 
-    print(f'The maximum subjectivity is: {max_subjectivity:.4f}')
-    print(f'The minimum subjectivity is: {min_subjectivity:.4f}')
-    print(f'The average subjectivity is: {avg_subjectivity / len(data):.4f}\n')
+    if DEBUG:
+        print(f'The maximum positive sentiment is: {max_sentiment:.4f}')
+        print(f'The minimum negative sentiment is: {min_sentiment:.4f}')
+        print(f'The average sentiment is: {avg_sentiment / len(data):.4f}\n')
+
+        print(f'The maximum subjectivity is: {max_subjectivity:.4f}')
+        print(f'The minimum subjectivity is: {min_subjectivity:.4f}')
+        print(f'The average subjectivity is: {avg_subjectivity / len(data):.4f}\n')
 
     return (max_sentiment, min_sentiment, avg_sentiment / len(data)), (max_subjectivity, min_subjectivity, avg_subjectivity / len(data))
 
@@ -53,10 +121,15 @@ def main():
     print('\n')
 
     # check the sentiment of the data
-    print('human sentiment analysis:')
-    get_sentiment_analysis(human)
-    print('machine sentiment analysis:')
+    polarity, subjectivity =  get_sentiment_analysis(human)
     get_sentiment_analysis(machine)
+
+    comparison_data: Tuple[float, float, float, float] = (polarity[0], polarity[1], subjectivity[0], subjectivity[1])
+
+    # get the prompt data
+    prompts = parse_promt_data(Path('prompts.jsonl'))
+
+    write_sentiment_results(prompts, comparison_data)
 
 
 if __name__ == '__main__':
