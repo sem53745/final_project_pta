cfabd40956a2d2dd5e0146c61573939723cb6880
tiemeboerema <tiemeboerema@gmail.com>
2024-06-02 10:11:41 +0200

    Merge conflict finally solved, added a bunch of comments and removed redundant variables in semantics.py

--- semantics.py
+++ semantics.py
@@ -2,8 +2,8 @@
 # Date: 01/06
 # Contributors: Joris van Bruggen (s5723752), Mervyn Bolhuis (s5119103), Tieme Boerema (s5410762), Jasper Kleine (s5152372), Sem Bartels (s5374588)
 
-from preprocessor import get_and_parse_texts, Path, parse_prompt_data
-from typing import Tuple, List, Dict, Literal
+from preprocessor import get_and_parse_texts, Path, parse_promt_data
+from typing import Tuple, List, Dict, NewType
 from spacy.tokens import Doc
 from fastcoref import spacy_component
 from collections import Counter
@@ -12,6 +12,11 @@ import nltk
 
 
 def perform_analysis(texts: List[Doc]):
+    ''' This function performs a few analyses on each doc within a list of docs.
+       These are: calculating the amount of references per coreference cluster,
+       calculating the average amount of Named Entities per sentence, and
+       calculating the average synsets per verb. These values are then returned,
+       to be used to compute the separator values. '''
 
     total_reference_amount = 0
     total_coref_amount = 0
@@ -20,6 +25,8 @@ def perform_analysis(texts: List[Doc]):
     amount_of_synsets = 0
     amount_of_verbs = 0
 
+    # For each doc, retrieve a bunch of values, and add them to their specific total.
+    # These totals are then used to calculate the variables that will be returned by this function.
     for doc in texts:
         coref_amount, reference_amount, sentence_amount, NE_amount, verb_amount, synset_amount = perform_analysis_single(doc)
         amount_of_sentences += sentence_amount
@@ -29,6 +36,7 @@ def perform_analysis(texts: List[Doc]):
         amount_of_verbs += verb_amount
         amount_of_synsets += synset_amount
 
+    # Calculate values which will later be used to compute separator values.
     references_per_cluster = total_reference_amount / total_coref_amount
     average_NE_sentence = amount_of_NEs / amount_of_sentences
     synsets_per_verb = amount_of_synsets / amount_of_verbs
@@ -37,24 +45,23 @@ def perform_analysis(texts: List[Doc]):
 
 
 def perform_analysis_single(doc:Doc):
+    ''' This function is similar to perform_analysis, but is specific to a single doc.
+       The main difference lies in the fact that this single analysis function is used
+       to analyze test data one by one, while the average values calculated in
+       perform_analysis are calculated across all of the test data.'''
 
-    coref_amount = 0
-    reference_amount = 0
-    NE_amount = 0
-    sentence_amount = 0
+    # Retrieve a bunch of values, which are later compared to separator values
+    # if test data is used, in order to determine whether a text is human or AI.
+    # If the function is used by perform_analysis, the data is used to create these separators.
     synset_amount = 0
-    verb_amount = 0
-
-    sentence_amount += len(list(doc.sents))
-    NE_amount += len(list(doc.ents))
-    coref_amount += len(list(doc._.coref_clusters))
-    reference_amount += len(list(reference for cluster in doc._.coref_clusters for reference in cluster))
-    #synset_amount += len(list(wn.synsets(word.lemma_, pos=wn.VERB) for word in doc if word.pos_ == "VERB"))
-    verb_amount += len(list(word for word in doc if word.pos_ == "VERB"))
-
+    sentence_amount = len(list(doc.sents))
+    NE_amount = len(list(doc.ents))
+    coref_amount = len(list(doc._.coref_clusters))
+    reference_amount = len(list(reference for cluster in doc._.coref_clusters for reference in cluster))
+    verb_amount = len(list(word for word in doc if word.pos_ == "VERB"))
+    
     for word in doc:
         if word.pos_ == "VERB":
-            #verb_amount += 1
             lemma = word.lemma_
             synsets = wn.synsets(lemma, pos=wn.VERB)
             synset_amount += len(synsets)
@@ -63,10 +70,16 @@ def perform_analysis_single(doc:Doc):
 
 
 def do_semantic_analysis(human_texts: List[Doc], machine_texts: List[Doc]):
-
+    ''' This function uses values calculated by perform_analysis 
+       to calculate separator values based on the average of the human
+       and machine text values.'''
+    
+    # Retrieve values for both machine and human texts
+    # which will then be used to calculate separator values.
     machine_reference_amount, machine_NE_sentence, machine_synsets_verb = perform_analysis(machine_texts)
     human_reference_amount, human_NE_sentence, human_synsets_verb = perform_analysis(human_texts)
 
+    # Calculate separator values for each classification category.
     separator_value_NE_sentence = (machine_NE_sentence + human_NE_sentence) / 2
     separator_value_references = (machine_reference_amount + human_reference_amount) / 2
     separator_value_synsets_verb = (machine_synsets_verb + human_synsets_verb) / 2
@@ -75,6 +88,11 @@ def do_semantic_analysis(human_texts: List[Doc], machine_texts: List[Doc]):
 
 
 def human_or_ai(value_to_divide_by:int, value_to_divide:int, separator:float):
+    ''' This function is used for each classification category. It calculates a value
+       comparable to the separator value for that specific category, and then
+       compares it to the separator. In each case, if the value is higher than
+       the separator, it is more likely to be a human text and human is returned.
+       Otherwise, AI is returned.'''
 
     if value_to_divide_by != 0:
         if (value_to_divide / value_to_divide_by) >= separator:
@@ -83,10 +101,11 @@ def human_or_ai(value_to_divide_by:int, value_to_divide:int, separator:float):
             return "AI"
 
 
-def get_semantic_results(seperators:tuple[float, float, float], prompts: List[Dict[str, Doc | str]]) -> List[Tuple[Literal['Human', 'Unsure', 'AI'], float]]:
+def get_semantic_results(separators:tuple[float, float, float], prompts: List[Dict[str, Doc | str]]):
+
     human_counter = 0
     ai_counter = 0
-    separator_NE_sentence, separator_references, separator_synsets_verb = seperators
+    separator_NE_sentence, separator_references, separator_synsets_verb = separators
     answers = []
     for prompt in prompts:
         coref_current, references_current, sentences_current, NE_current, verbs_current, synsets_current = perform_analysis_single(prompt['text'])
@@ -106,36 +125,36 @@ def get_semantic_results(seperators:tuple[float, float, float], prompts: List[Di
         else:
             ai_counter += 1
 
-        max_score = 3
-        guessed = max(human_counter, ai_counter)
-        certainty = guessed / max_score
+        # The commented code within the next block of code can be uncommented in order to
+        # see the true labels of the texts, followed by the semantic classifiers labels.
         if human_counter > ai_counter:
-            answers.append(("Human", certainty))
-            #print(prompt['by'], end = ", ")
-            #print("Human")
+            answers.append("Human")
+            print(prompt['by'], end = ", ")
+            print("Human")
         elif human_counter == ai_counter:
-            answers.append(("Unsure", certainty))
-            #print(prompt['by'], end = ", ")
-            #print("Unsure")
+            answers.append("Unsure")
+            print(prompt['by'], end = ", ")
+            print("Unsure")
         else:
-            answers.append(("AI", certainty))
-            #print(prompt['by'], end = ", ")
-            #print("AI")
-
-        human_counter = 0
-        ai_counter = 0
-
-
+            answers.append("AI")
+            print(prompt['by'], end = ", ")
+            print("AI")
 
     return answers
 
 
 def main():
 
+    # Load training data
     human_text, machine_text = get_and_parse_texts(Path('human.jsonl'), Path('group1.jsonl'))
+    
+    # Compute the three separator values
     NE_sentence_separator, references_amount_separator, synsets_verb_separator = do_semantic_analysis(human_text, machine_text)
-
-    prompts = parse_prompt_data(Path('prompts.jsonl'))
+    
+    # Load test data.
+    prompts = parse_promt_data(Path('prompts.jsonl'))
+    
+    # Compute results
     results = get_semantic_results((NE_sentence_separator, references_amount_separator, synsets_verb_separator), prompts)
 
 
